6f1b37566caf5eaf4b00feb6ac0f7f996225631b
omarcat <omarelnadi35@gmail.com>
2025-02-27 15:30:38 +0200

    Hello Github

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  char name[10];
+   printf ("please enter your first name: ");
+   scanf("%s",name);
+   printf("Congrats %s You Learned How To Use Github",name );
+
+    return 0;
+}
