5426875617f20c7016d17580eeea8967edfb0b83
omarcat <omarelnadi35@gmail.com>
2025-03-14 22:44:19 +0200

    Update 1.c

--- Week.03/1.c
+++ Week.03/1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// this code is used to find the maximum between 2 numbers using a function
 int main()
 {
     int num1,num2;
@@ -8,9 +8,9 @@ int main()
     scanf("%d",&num1);
     printf("enter the second number: ");
     scanf("%d",&num2);
-    printf("the bigger number is:%d",sum(num1,num2));
+    printf("the bigger number is:%d",max(num1,num2));
 }
-int sum(int num1,int num2)
+int max(int num1,int num2)
 {
     if (num1>num2)
         return num1;
