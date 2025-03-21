e15af4f020a50e64b35f3d587d27af1c9bd07d0b
omarcat <omarelnadi35@gmail.com>
2025-03-21 16:31:04 +0200

    Update 5.c

--- Week.04/5.c
+++ Week.04/5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// this code prints the row and column sums of a 5x5 array
 int main()
 {
   int a[5][5] = {{8,3,9,0,10},{3,5,17,1,1},{2,8,6,23,1},{15,7,3,2,9},{6,14,2,6,0}
